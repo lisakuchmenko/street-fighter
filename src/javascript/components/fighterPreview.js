d817c78ca6c57cee9abd9cf124c587a975df96c0
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2022-06-10 19:18:51 +0200

    implemented fighter info preview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,29 +1,48 @@
 import { createElement } from '../helpers/domHelper';
 
 export function createFighterPreview(fighter, position) {
-  const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-  const fighterElement = createElement({
-    tagName: 'div',
-    className: `fighter-preview___root ${positionClassName}`,
-  });
+	const positionClassName =
+		position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+	const fighterElement = createElement({
+		tagName: 'div',
+		className: `fighter-preview___root ${positionClassName}`,
+	});
 
-  // todo: show fighter info (image, name, health, etc.)
+	if (fighter) {
+		const fighterImg = createFighterImage(fighter);
+		const fighterName = createElement({ tagName: 'div' });
+		const fighterDetails = createElement({
+			tagName: 'div',
+			className: 'fighter-preview__details',
+		});
+		const fighterDetailsWrapper = createElement({
+			tagName: 'div',
+			className: 'fighter-preview__details-wrapper',
+		});
+		fighterDetails.innerHTML = `
+			Health:
+			${fighter.health}
+		`;
+		fighterName.innerText = fighter.name;
+		fighterDetailsWrapper.append(fighterName, fighterDetails);
+		fighterElement.append(fighterImg, fighterDetailsWrapper);
+	}
 
-  return fighterElement;
+	return fighterElement;
 }
 
 export function createFighterImage(fighter) {
-  const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
-    title: name,
-    alt: name 
-  };
-  const imgElement = createElement({
-    tagName: 'img',
-    className: 'fighter-preview___img',
-    attributes,
-  });
+	const { source, name } = fighter;
+	const attributes = {
+		src: source,
+		title: name,
+		alt: name,
+	};
+	const imgElement = createElement({
+		tagName: 'img',
+		className: 'fighter-preview___img',
+		attributes,
+	});
 
-  return imgElement;
+	return imgElement;
 }
--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -1,21 +1,26 @@
 import { callApi } from '../helpers/apiHelper';
 
 class FighterService {
-  #endpoint = 'fighters.json';
+	#endpoint = 'fighters.json';
 
-  async getFighters() {
-    try {
-      const apiResult = await callApi(this.#endpoint);
-      return apiResult;
-    } catch (error) {
-      throw error;
-    }
-  }
+	async getFighters() {
+		try {
+			const apiResult = await callApi(this.#endpoint);
+			return apiResult;
+		} catch (error) {
+			throw error;
+		}
+	}
 
-  async getFighterDetails(id) {
-    // todo: implement this method
-    // endpoint - `details/fighter/${id}.json`;
-  }
+	async getFighterDetails(id) {
+		try {
+			const endpoint = `details/fighter/${id}.json`;
+			const apiResult = await callApi(endpoint, 'GET');
+			return apiResult;
+		} catch (error) {
+			throw error;
+		}
+	}
 }
 
 export const fighterService = new FighterService();
